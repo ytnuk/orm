432a7e699a37043b7c4150b1e571c675c05c4491
Vít Kutný <vitkutny@outlook.com>
2014-11-01 14:31:53 +0100

    init

---
+++ src/Orm/Extension.php
@@ -0,0 +1,55 @@
+<?php
+namespace WebEdit\Orm;
+
+use Nextras\Orm;
+use WebEdit\Config;
+use WebEdit\Database;
+
+/**
+ * Class Extension
+ *
+ * @package WebEdit\Orm
+ */
+final class Extension extends Orm\DI\OrmExtension implements Config\Provider
+{
+
+	/**
+	 * @var array
+	 */
+	private $defaults = [
+		'model' => Database::class,
+		'repositories' => []
+	];
+
+	/**
+	 * @return array
+	 */
+	public function getConfigResources()
+	{
+		return [self::class => $this->defaults];
+	}
+
+	/**
+	 * @param string $model
+	 *
+	 * @return array
+	 */
+	protected function getRepositoryList($model)
+	{
+		$config = $this->getConfig($this->defaults);
+		$repositories = parent::getRepositoryList($model);
+		foreach ($config['repositories'] as $name => $repository) {
+			$repositories[] = [
+				'name' => $name,
+				'serviceName' => $this->prefix('repositories.' . $name),
+				'class' => $repository,
+				'entities' => call_user_func([
+					$repository,
+					'getEntityClassNames'
+				]),
+			];
+		}
+
+		return $repositories;
+	}
+}
