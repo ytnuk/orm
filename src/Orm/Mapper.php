043690035ef349ed481befe6e1ed2edf6df2060c
Vít Kutný <vitkutny@outlook.com>
2015-01-08 01:57:45 +0100

    dev

--- src/Orm/Mapper.php
+++ src/Orm/Mapper.php
@@ -20,7 +20,7 @@ abstract class Mapper extends Nextras\Orm\Mapper\Mapper
 		if ( ! $this->tableName) {
 			$namespace = explode('\\', $this->reflection->getNamespaceName());
 			array_shift($namespace);
-			$this->tableName = implode('_', array_map('lcfirst', $namespace));
+			$this->tableName = Nextras\Orm\StorageReflection\DbStorageReflection::underscore(implode($namespace));
 		}
 
 		return $this->tableName;
