14038db1affe6626b5fbecab1abf70c18765c776
Vít Kutný <vitkutny@icloud.com>
2015-05-27 09:12:27 +0200

    dev

--- src/Orm/Entity.php
+++ src/Orm/Entity.php
@@ -3,13 +3,14 @@
 namespace Ytnuk\Orm;
 
 use Nextras;
+use Ytnuk;
 
 /**
  * Class Entity
  *
  * @package Ytnuk\Orm
  */
-abstract class Entity extends Nextras\Orm\Entity\Entity
+abstract class Entity extends Nextras\Orm\Entity\Entity implements Ytnuk\Cache\Provider
 {
 
 	const PROPERTY_NAME = 'id';
@@ -25,5 +26,43 @@ abstract class Entity extends Nextras\Orm\Entity\Entity
 		}
 
 		return (string) $this->getValue($property);
+	}
+
+	/**
+	 * @param bool $invalidate
+	 *
+	 * @inheritdoc
+	 */
+	public function getCacheTags($invalidate = FALSE)
+	{
+		$tags = [
+			$this->getCacheKey()
+		];
+		if ($invalidate) {
+			foreach ($this->getMetadata()->getProperties() as $property) {
+				if ( ! $property->relationshipIsMain && $property->relationshipType === Nextras\Orm\Entity\Reflection\PropertyMetadata::RELATIONSHIP_ONE_HAS_ONE_DIRECTED) {
+					if ($relationEntity = $this->getValue($property->name)) {
+						if ($relationEntity instanceof Ytnuk\Cache\Provider) {
+							$tags = array_merge($tags, $relationEntity->getCacheTags($invalidate));
+						}
+					}
+				}
+			}
+		} else {
+			$tags[] = get_class($this);
+		}
+
+		return $tags;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function getCacheKey()
+	{
+		return implode('::', [
+			get_class($this),
+			$this->id
+		]);
 	}
 }
--- src/Orm/Model.php
+++ src/Orm/Model.php
@@ -3,6 +3,8 @@
 namespace Ytnuk\Orm;
 
 use Nextras;
+use Nette;
+use Ytnuk;
 
 /**
  * Class Database
@@ -12,4 +14,21 @@ use Nextras;
 final class Model extends Nextras\Orm\Model\Model
 {
 
+	/**
+	 * @inheritdoc
+	 */
+	public function __construct(array $configuration, Nextras\Orm\Model\IRepositoryLoader $repositoryLoader, Nextras\Orm\Model\MetadataStorage $metadataStorage, Nette\Caching\IStorage $storage)
+	{
+		parent::__construct($configuration, $repositoryLoader, $metadataStorage);
+		$this->onFlush[] = function ($persisted, $removed) use ($storage) {
+			array_map(function (Nextras\Orm\Entity\IEntity $entity) use ($storage) {
+				if ($entity instanceof Ytnuk\Cache\Provider) {
+					$storage->remove($entity->getCacheKey());
+					$storage->clean([
+						Nette\Caching\Cache::TAGS => $entity->getCacheTags(TRUE)
+					]);
+				}
+			}, $persisted, $removed);
+		};
+	}
 }
