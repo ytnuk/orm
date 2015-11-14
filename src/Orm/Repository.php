16041fd17ac53774f76ba0f1ffabc8a8bd8faaf6
Vít Kutný <vitkutny@icloud.com>
2015-11-14 23:47:04 +0100

    I don`t think this is needed anymore

--- src/Orm/Repository.php
+++ src/Orm/Repository.php
@@ -7,35 +7,6 @@ abstract class Repository
 	extends Nextras\Orm\Repository\Repository
 {
 
-	public function remove(
-		$entity,
-		$recursive = FALSE
-	) : Nextras\Orm\Entity\IEntity
-	{
-		//TODO: is this needed anymore?
-		if ($recursive) {
-			foreach (
-				$entity->getMetadata()->getProperties() as $property
-			) {
-				if ($property->relationship && $property->relationship->isMain && $property->relationship->type === Nextras\Orm\Entity\Reflection\PropertyRelationshipMetadata::ONE_HAS_ONE) {
-					if ($entity->hasValue($property->name) && $relationEntity = $entity->getValue($property->name)) {
-						if ($relationEntity->isAttached()) {
-							$relationEntity->getRepository()->remove(
-								$relationEntity,
-								$recursive
-							);
-						}
-					}
-				}
-			}
-		}
-
-		return parent::remove(
-			$entity,
-			$recursive
-		);
-	}
-
 	public function setModel(Nextras\Orm\Model\IModel $model)
 	{
 		parent::setModel($model);
