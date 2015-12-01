c7a27c516a6cfa130466aaf271f0b74512ba6163
Vít Kutný <vitkutny@icloud.com>
2015-12-01 21:19:37 +0100

    refactor control entity setters

--- src/Orm/Control.php
+++ src/Orm/Control.php
@@ -30,17 +30,6 @@ abstract class Control
 		$this->entity = $entity;
 	}
 
-	public function setEntity(Nextras\Orm\Entity\IEntity $entity)
-	{
-		$this->entity = $entity;
-		array_walk(
-			iterator_to_array($this->getComponents()),
-			function (Nette\ComponentModel\IComponent $component) {
-				$this->removeComponent($component);
-			}
-		);
-	}
-
 	public function setCacheStorage(Nette\Caching\IStorage $storage)
 	{
 		parent::setCacheStorage($storage);
